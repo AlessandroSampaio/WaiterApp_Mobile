1134766504b04797fa4815edb59ac001486bb586
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-11-21 08:39:35 -0300

    Adiciona efeito de seleção a lista de categorias

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -2,8 +2,17 @@ import { FlatList } from 'react-native';
 import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 import { Category, Icon } from './styles';
+import { useState } from 'react';
 
 export function Categories() {
+
+  const [selectedCategpry, setSelectedCategory] = useState<string | null>(null);
+
+
+  function handleSelectCategory(categoryId: string) {
+    setSelectedCategory(prevState => prevState === categoryId ? null : categoryId);
+  }
+
   return (
     <FlatList
       horizontal
@@ -11,16 +20,21 @@ export function Categories() {
       showsHorizontalScrollIndicator={false}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={(category) => category._id}
-      renderItem={({ item }) => (
-        <Category key={item._id}>
+      renderItem={({ item }) => {
+
+        const isSelected = selectedCategpry === item._id;
+
+        return (
+        <Category key={item._id} onPress={() => handleSelectCategory(item._id)}>
           <Icon>
-            <Text>{item.icon}</Text>
+            <Text opacity={isSelected ? 1 : 0.5} >{item.icon}</Text>
           </Icon>
           <Text size={14} weight={600}>
             {item.name}
           </Text>
         </Category>
-      )}
+        );
+      }}
     />
   );
 }
