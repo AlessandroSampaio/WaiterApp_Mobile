aee8d2125e1c02ee558eaacfbea4e4e1f91be2b9
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-11-23 09:03:14 -0300

    Cria o componente Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,39 @@
+import { FlatList, TouchableOpacity } from 'react-native';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+import { Product, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
 export function Menu() {
   return (
-    null
+    <FlatList
+      style={{ marginTop: 32}}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      data={products}
+      keyExtractor={product => product._id}
+      ItemSeparatorComponent={Separator}
+      renderItem={({item: product}) => (
+        <Product
+          onPress={() => (null)}
+        >
+          <ProductImage
+            source={{
+              uri: `http://192.168.18.227:3001/uploads/${product.imagePath}`
+            }}
+          />
+
+          <ProductDetails>
+            <Text weight={600}>{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }} >{product.description}</Text>
+            <Text size={14} weight={600}>{formatCurrency(product.price)}</Text>
+          </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+    />
   );
 }
